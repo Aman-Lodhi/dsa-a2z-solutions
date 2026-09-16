6bf5250ff3ee0cb58034c61ab8f60b198b1bd5e1
Aman <amanlodhi060@gmail.com>
2026-09-16 12:26:10 +0530

    Hashing

---
+++ Basic/Hashing/highest.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include <unordered_map>
+using namespace std;
+
+int highest(vector<int> &arr){
+    int value = INT_MIN;
+    int most = 0;
+    unordered_map<int,int> mpp;
+    for(int i=0;i<arr.size();i++){
+         mpp[arr[i]]++;
+    }
+    for(auto x: mpp){
+      int  maxi = x.first;
+      int can = x.second;
+      if(can>most){
+        most=can;
+        value = maxi;
+      }else if(most==can){
+        value = max(value,most);
+      }
+    }
+    return value;
+}
+
+int main(){
+    vector<int> arr = {2,6,2,8,9,5,6,8,9,12,42,3,2,21};
+    cout<<"Highest occurense in array :" << highest(arr);
+}
